0b5347c36df0757ad79afa5ceb75bab2f66da47a
jez2cool <aka2cool@gmail.com>
2012-12-01 03:13:43 -0500

    Completed Exercise 3

---
+++ Exercise3-2.py
@@ -0,0 +1,14 @@
+# Praction with Floating numbers
+ 
+# Addtion Problem 
+print "How much is 3/4 and 2/3 together?"
+raw_input("The Answer is ") 
+print 0.75 + 0.66
+
+# Subration Problem
+print "I Had 16 of 19 items in my chest and I lost one fifth of them. How many did I Lose?"
+raw_input()
+print 0.842 - 0.25
+
+
+
