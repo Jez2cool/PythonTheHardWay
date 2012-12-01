3348efa91f3f3d7e3495065154d9c0b417bbec66
jez2cool <aka2cool@gmail.com>
2012-12-01 02:00:43 -0500

    Add folders

---
+++ Exercise2/Exercise2.py
@@ -0,0 +1,19 @@
+# Goal is to enter commits into the program  
+# Read bottom up 
+
+print "Hey" # SIGHS!
+raw_input() 
+print "I am" # Because I will come back and edit later
+raw_input() 
+print "Trying" # Typing I will just continue to go by 
+raw_input()
+print "To learn" #  So I am not going stop  
+raw_input() 
+print "Something" # Cant spend alot of time on this  
+raw_input()
+print "New" # This is to be read from bottom up
+raw_input()
+print "!" # Trying to get this stuff done 
+
+
+
