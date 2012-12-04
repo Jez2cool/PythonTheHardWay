3a991854f93fd15c88af53bb3327a857a82e5e38
jez2cool <aka2cool@gmail.com>
2012-12-04 10:29:41 -0500

    Excercise 5&6

---
+++ Exercise5/Exercise5.py
@@ -0,0 +1,26 @@
+my_name = 'Jordan'
+my_age = 24 # My age 
+my_height = 71 # inches
+my_weight = 165 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+my_race = 'Black'
+# Add pauses to code 
+print "Let's talk about %s." % my_name
+raw_input()
+print "He is and %s Man." % my_race
+raw_input()
+print "He's %d inches tall." % my_height
+raw_input()
+print "He's %d pounds heavy." % my_weight
+raw_input()
+print "Actually that's not too heavy."
+raw_input()
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+raw_input()
+print "His teeth are usually %s depending on the his smoking habit." % my_teeth
+raw_input()
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
---
+++ Exercise6/Exercise6.py
@@ -0,0 +1,23 @@
+#Exercise 6 practice 
+
+# Varibles 
+x = "There are %r types of people." % 10
+bin = "binary"
+dn = "don't"
+y = "Those who know %s and those who %s." % (bin, dn)
+
+# Prints x and y .
+print x
+print y 
+
+
+print "I said: %r." % x
+print "I also said: '%s'." % y 
+
+hilarious = False
+# joke_evaluation = % hilarious
+# i dont know what this line is for seems pointless 
+w = "This is the left side of..."
+e = "a string with a right side."
+# With Plus sign it print both in 1 line 
+print w + e 
